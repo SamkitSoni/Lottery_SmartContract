02c4c5951c2872e7a8a60112ce7f11c6b8648726
SamkitSoni <samkitsoni09@gmail.com>
2025-03-18 08:28:35 +0000

    Added unit test file

--- script/Counter.s.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {Script, console} from "forge-std/Script.sol";
-import {Raffle} from "../src/Raffle.sol";
-
-contract RaffleScript is Script {}
--- test/Counter.t.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {Test, console} from "forge-std/Test.sol";
-import {Raffle} from "../src/Raffle.sol";
-
-contract RaffleTest is Test {}
